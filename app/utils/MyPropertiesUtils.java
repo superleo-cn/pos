8ea3e470271ea1165a63e291dec1724fbf17aab9
LEO HU <superleo.peking@gmail.com>
2013-11-13 01:47:53 +0800

    Login 8

--- app/utils/MyPropertiesUtils.java
+++ app/utils/MyPropertiesUtils.java
@@ -3,16 +3,24 @@ package utils;
 import java.beans.PropertyDescriptor;
 
 import org.apache.commons.beanutils.PropertyUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import controllers.Auth;
 
 public class MyPropertiesUtils {
+	
+	final static Logger logger = LoggerFactory.getLogger(MyPropertiesUtils.class);
 
 	public static void copyProperties(Object destBean, Object srcBean) {
 		PropertyDescriptor[] descriptors = PropertyUtils.getPropertyDescriptors(srcBean);
 		if (descriptors != null) {
+			logger.info("[System]-[Info]-[User fields size is {}]", new Object[] { descriptors.length });
 			for (PropertyDescriptor descriptor : descriptors) {
 				try {
 					String propertyName = descriptor.getName();
 					Object val = PropertyUtils.getProperty(srcBean, propertyName);
+					logger.info("[System]-[Info]-[User field({}) value({})]", new Object[] { propertyName, val });
 					if (val != null) {
 						PropertyUtils.setProperty(destBean, propertyName, val);
 					}
