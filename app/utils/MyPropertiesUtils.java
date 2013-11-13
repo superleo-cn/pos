6dd4d10750d97d59d65f827b46d60edf6ea676a8
LEO HU <superleo.peking@gmail.com>
2013-11-13 17:37:55 +0800

    Use Spring BeanUtils

--- app/utils/MyPropertiesUtils.java
+++ app/utils/MyPropertiesUtils.java
@@ -1,31 +1,46 @@
 package utils;
 
 import java.beans.PropertyDescriptor;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 
-import org.apache.commons.beanutils.PropertyUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.FatalBeanException;
 
-import controllers.Auth;
+public class MyPropertiesUtils extends BeanUtils {
 
-public class MyPropertiesUtils {
-	
 	final static Logger logger = LoggerFactory.getLogger(MyPropertiesUtils.class);
 
-	public static void copyProperties(Object destBean, Object srcBean) {
-		PropertyDescriptor[] descriptors = PropertyUtils.getPropertyDescriptors(srcBean);
-		if (descriptors != null) {
-			logger.info("[System]-[Info]-[User fields size is {}]", new Object[] { descriptors.length });
-			for (PropertyDescriptor descriptor : descriptors) {
-				try {
-					String propertyName = descriptor.getName();
-					Object val = PropertyUtils.getProperty(srcBean, propertyName);
-					logger.info("[System]-[Info]-[User field({}) value({})]", new Object[] { propertyName, val });
-					if (val != null) {
-						PropertyUtils.setProperty(destBean, propertyName, val);
+	public static void copyProperties(Object target, Object source) throws BeansException {
+
+		Class<?> actualEditable = target.getClass();
+		PropertyDescriptor[] targetPds = getPropertyDescriptors(actualEditable);
+		logger.info("[System]-[Debug]-[MyPropertiesUtils ({}) size is ({})]", new Object[] { actualEditable,
+				targetPds.length });
+		for (PropertyDescriptor targetPd : targetPds) {
+			logger.info("[System]-[Debug]-[MyPropertiesUtils peroperty is ({})]", new Object[] { targetPd.getName()});
+			if (targetPd.getWriteMethod() != null) {
+				PropertyDescriptor sourcePd = getPropertyDescriptor(source.getClass(), targetPd.getName());
+				if (sourcePd != null && sourcePd.getReadMethod() != null) {
+					try {
+						Method readMethod = sourcePd.getReadMethod();
+						if (!Modifier.isPublic(readMethod.getDeclaringClass().getModifiers())) {
+							readMethod.setAccessible(true);
+						}
+						Object value = readMethod.invoke(source);
+						if (value != null) {
+							Method writeMethod = targetPd.getWriteMethod();
+							if (!Modifier.isPublic(writeMethod.getDeclaringClass().getModifiers())) {
+								writeMethod.setAccessible(true);
+							}
+							writeMethod.invoke(target, value);
+						}
+					} catch (Throwable ex) {
+						throw new FatalBeanException("Could not copy properties from source to target", ex);
 					}
-				} catch (Exception ignore) {
-					// not interested in what we can't read or write
 				}
 			}
 		}
