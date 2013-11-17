65bdd8b33e1dd6c66f4981cd0efd686fa05a57a4
LEO HU <superleo.peking@gmail.com>
2013-11-17 21:37:17 +0800

    Update view.sql

--- view.sql
+++ view.sql
@@ -28,31 +28,24 @@ AND s.id = t.shop_id;
 
 --Report 2
 SELECT
-    s.id,
-    s.name               AS shop_name,
-    u.username           AS username,
-    f.name               AS food_name,
-    f.name_zh            AS food_name_zh,
-    t.cost_price         AS cost_price,
-    t.retail_price       AS retail_price,
-    t.quantity           AS quantity,
-    t.total_cost_price   AS total_cost_price,
-    t.total_discount     AS total_discount,
-    t.total_package      AS total_package,
-    t.free_of_charge     AS free_of_charge,
-    t.total_retail_price AS total_retail_price,
-    t.create_date        AS create_date
+    s.id AS shop_id,
+    u.id AS user_id,
+    d.a_open_balance,
+    d.b_expenses,
+    d.c_cash_collected,
+    d.d_daily_turnover,
+    d.e_next_open_balance,
+    f_bring_back_cash,
+    d.create_date
 FROM
     tb_shop s,
     tb_user u,
-    tb_food f,
-    tb_transaction t
+    tb_daily_summary d
 WHERE
     s.id = u.shop_id
-AND s.id = f.shop_id
-AND u.id = t.user_id
-AND f.id = t.food_id
-AND s.id = t.shop_id;
+AND s.id = d.shop_id
+AND u.id = d.user_id
+AND d.create_date BETWEEN '2013-11-01' AND '2013-12-31';
 
 --P & L we need 2 sqls, Net Profit (A-B-C)
 --sql 1
