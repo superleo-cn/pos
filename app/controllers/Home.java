a2f9765c4fb31a4924f734448e090e87df521e5b
Wiyanto Ngasinur <wngasinur@gmail.com>
2013-11-18 23:53:04 +0800

    Fishball

--- app/controllers/Home.java
+++ app/controllers/Home.java
@@ -11,7 +11,7 @@ import constants.Pages;
 public class Home extends Basic {
 
     public static void index() {
-
+        if(session.get(Constants.CURRENT_USERID)==null) redirect("/login");
         render(Pages.HOME);
     }
 
