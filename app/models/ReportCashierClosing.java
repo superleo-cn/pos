578c267e37905b958721eceb02d6e0b313e957c7
Wiyanto Ngasinur <wngasinur@gmail.com>
2014-01-15 00:07:24 +0800

    Fishball

--- app/models/ReportCashierClosing.java
+++ app/models/ReportCashierClosing.java
@@ -131,4 +131,8 @@ public class ReportCashierClosing {
     public Double getTotal() {
         return total;
     }
+
+    public Double getNextOpenBalance() {
+        return nextOpenBalance;
+    }
 }
