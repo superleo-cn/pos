396bc8a2320d1ec44cfc2466eed7623384c56924
LEO HU <superleo.peking@gmail.com>
2014-04-08 16:25:59 +0800

    Commit.

--- app/controllers/Foods.java
+++ app/controllers/Foods.java
@@ -22,6 +22,22 @@ public class Foods extends Basic {
 	public static void listJson(Long id) {
 		Map result = new HashMap();
 		try {
+			result.put(Constants.CODE, Constants.SUCCESS);
+			result.put(Constants.DATAS, Food.listByShop(id));
+		} catch (Exception e) {
+			Shop shop = Shop.view(id);
+			String shopName = StringUtils.defaultIfEmpty(shop.name, Constants.NA);
+			result.put(Constants.CODE, Constants.ERROR);
+			result.put(Constants.MESSAGE, Messages.FOOD_LIST_ERROR);
+			logger.error(Messages.FOOD_LIST_ERROR_MESSAGE, new Object[] { shopName, e });
+		}
+		renderJSON(result);
+
+	}
+	
+	public static void listExtJson(Long id) {
+		Map result = new HashMap();
+		try {
 			Map datas = new HashMap();
 			datas.put("Foods", Food.listByShop(id));
 			datas.put("Categories", Category.listByShop(id));
