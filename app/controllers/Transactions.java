5322839f499ae66578e40175c152b54be3cfc7bb
LEO HU <superleo.peking@gmail.com>
2014-10-28 23:33:01 +0800

    OK

--- app/controllers/Transactions.java
+++ app/controllers/Transactions.java
@@ -32,7 +32,7 @@ public class Transactions extends Basic {
 			if (CollectionUtils.size(transactions) > 0) {
 				List datas = new ArrayList();
 				for (Transaction transaction : transactions) {
-					str += "[date = " + new Date() + "], [transactionId = " + transaction.transactionId
+					str = "[date = " + new Date() + "], [transactionId = " + transaction.transactionId
 							+ "], [androidId = " + transaction.androidId + "], [shopId = " + transaction.shop.id
 							+ "], [userId = " + transaction.user.id + "], [quantity = " + transaction.quantity
 							+ "], [foodId = " + transaction.food.id + "], [totalDiscount = "
