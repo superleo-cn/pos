9e5f1eada4abd5faffd885f4f42c0ae6e607eec5
LEO HU <superleo.peking@gmail.com>
2014-11-30 11:38:16 +0800

    Refactor code.

--- public/app/main.privia.js
+++ public/app/main.privia.js
@@ -1,172 +1,162 @@
 require.config({
-  baseUrl: '/app',
-  paths: {
-    "dataTables" : "../lib/datatables/jquery.dataTables.min",
-    "dataTables.sorting" :"../lib/datatables/jquery.dataTables.sorting",
-    "dataTables.bootstrap" : "../lib/datatables/jquery.dataTables.bootstrap.min",
-    "underscore" : "../lib/underscore",
-    "backbone" :"../lib/backbone-min",
-    "backbone.syphon" :"../lib/backbone.syphon",
-    "util" : "../util.nganterin",
-    "bootstrap" : "../bootstrap/js/bootstrap.min",
-    "bootstrap-timepicker" : "../lib/datepicker/bootstrap-timepicker.min",
-    "jquery" : "/js/jquery.min",
-    "jquery.ui" : "../lib/jquery-ui/jquery-ui-1.10.2.custom.min",
-    "jquery.inputmask" : "/js/forms/jquery.inputmask.min",
-    "jquery.validate" : "../lib/validation/jquery.validate.min",    
-    "jquery.serializeObject" : "/js/jquery.serializeObject",
-    "antiscroll" : "../lib/antiscroll/antiscroll.min",
-    "jquery.form" : "/js/forms/jquery.form",
-    "jquery.actual" : "/js/jquery.actual.min",
-    "jquery.fineuploader" : "../lib/jquery.fineuploader-3.5.0.min",    
-    "select2" : "../lib/select2.min",
-    "stepy" : "../lib/stepy/js/jquery.stepy.min",
-    "flot" : "../lib/flot/jquery.flot.min",
-    "flot.categories" : "../lib/flot/jquery.flot.categories.min",
-    "flot.time" : "../lib/flot/jquery.flot.time.min",
-    "flot.pie" : "../lib/flot/jquery.flot.pie.min",        
-    "qtip2" : "../lib/qtip2/jquery.qtip.min",
-    "text" : "/js/text",
-    "pickadate.picker" : "../lib/pickadate/picker",
-    "pickadate.picker.time" : "../lib/pickadate/picker.time",
-    "gebo_common" : "/js/gebo_common",
-    "moment" : "../lib/moment_js/moment.min",
-    "fusioncharts" : "../lib/fusioncharts/fusioncharts",
-    "router" : "router.privia",
-    "app" : "app.privia"
-  },
-  shim: {
-    'backbone': {
-      //These script dependencies should be loaded before loading
-      //backbone.js
-      deps: ['underscore', 'jquery'],
-      //Once loaded, use the global 'Backbone' as the
-      //module value.
-      exports: 'Backbone'
-    },
-    'underscore': {
-      exports: '_'
-    },
-     'gebo_common' : {
-      deps : ['jquery','jquery.actual']
-    },
-    'antiscroll' : {
-      deps : ['jquery']
-    },
-    'jquery.validate' : {
-      deps : ['jquery']
-    },
-    'jquery.form' : {
-      deps : ['jquery']
-    },
-    'jquery.actual' : {
-      deps : ['jquery']
-    },
-    'jquery.inputmask' : {
-      deps : ['jquery']
-    },
-    'jquery.ui' : {
-      deps : ['jquery']
-    },
-    'jquery.serializeObject' : {
-      deps : ['jquery']
-    },
-    'dataTables': {
-      exports: 'dataTable'
-    },
-    'dataTables.sorting' : {
-      deps : ['dataTables']
-    },
-    'dataTables.bootstrap' : {
-      deps : ['dataTables']
-    },
-    'bootstrap' : {
-      deps : ['jquery']
-    },
-      'bootstrap-timepicker' : {
-          deps : ['bootstrap']
-      },
-    'jquery.fineuploader' : {
-      deps : ['jquery']
-    },
-    'select2' : {
-      deps : ['jquery']
-    },
-    'stepy' : {
-      deps : ['jquery']
-    },
-    'flot' : {
-      deps : ['jquery']
-    },
-    'flot.categories' : {
-      deps : ['flot']
-    },
-    'flot.time' : {
-      deps : ['flot']
-    },
-    'flot.pie' : {
-      deps : ['flot']
-    },
-    'qtip2' : {
-      deps : ['jquery']
-    },
-    'backbone.syphon' : {
-      deps : ['backbone']
-    },
-    'pickadate.picker.time' : {
-        deps : ['pickadate.picker']
-    },
-    'fusioncharts' : {
-        deps : ['jquery']
-    }
-  }
+	baseUrl : '/app',
+	paths : {
+		"dataTables" : "../lib/datatables/jquery.dataTables.min",
+		"dataTables.sorting" : "../lib/datatables/jquery.dataTables.sorting",
+		"dataTables.bootstrap" : "../lib/datatables/jquery.dataTables.bootstrap.min",
+		"underscore" : "../lib/underscore",
+		"backbone" : "../lib/backbone-min",
+		"backbone.syphon" : "../lib/backbone.syphon",
+		"util" : "../util.nganterin",
+		"bootstrap" : "../bootstrap/js/bootstrap.min",
+		"bootstrap-timepicker" : "../lib/datepicker/bootstrap-timepicker.min",
+		"jquery" : "/js/jquery.min",
+		"jquery.ui" : "../lib/jquery-ui/jquery-ui-1.10.2.custom.min",
+		"jquery.inputmask" : "/js/forms/jquery.inputmask.min",
+		"jquery.validate" : "../lib/validation/jquery.validate.min",
+		"jquery.serializeObject" : "/js/jquery.serializeObject",
+		"antiscroll" : "../lib/antiscroll/antiscroll.min",
+		"jquery.form" : "/js/forms/jquery.form",
+		"jquery.actual" : "/js/jquery.actual.min",
+		"jquery.fineuploader" : "../lib/jquery.fineuploader-3.5.0.min",
+		"select2" : "../lib/select2.min",
+		"stepy" : "../lib/stepy/js/jquery.stepy.min",
+		"flot" : "../lib/flot/jquery.flot.min",
+		"flot.categories" : "../lib/flot/jquery.flot.categories.min",
+		"flot.time" : "../lib/flot/jquery.flot.time.min",
+		"flot.pie" : "../lib/flot/jquery.flot.pie.min",
+		"qtip2" : "../lib/qtip2/jquery.qtip.min",
+		"text" : "/js/text",
+		"pickadate.picker" : "../lib/pickadate/picker",
+		"pickadate.picker.time" : "../lib/pickadate/picker.time",
+		"gebo_common" : "/js/gebo_common",
+		"moment" : "../lib/moment_js/moment.min",
+		"fusioncharts" : "../lib/fusioncharts/fusioncharts",
+		"router" : "router.privia",
+		"app" : "app.privia"
+	},
+	shim : {
+		'backbone' : {
+			// These script dependencies should be loaded before loading
+			// backbone.js
+			deps : [ 'underscore', 'jquery' ],
+			// Once loaded, use the global 'Backbone' as the
+			// module value.
+			exports : 'Backbone'
+		},
+		'underscore' : {
+			exports : '_'
+		},
+		'gebo_common' : {
+			deps : [ 'jquery', 'jquery.actual' ]
+		},
+		'antiscroll' : {
+			deps : [ 'jquery' ]
+		},
+		'jquery.validate' : {
+			deps : [ 'jquery' ]
+		},
+		'jquery.form' : {
+			deps : [ 'jquery' ]
+		},
+		'jquery.actual' : {
+			deps : [ 'jquery' ]
+		},
+		'jquery.inputmask' : {
+			deps : [ 'jquery' ]
+		},
+		'jquery.ui' : {
+			deps : [ 'jquery' ]
+		},
+		'jquery.serializeObject' : {
+			deps : [ 'jquery' ]
+		},
+		'dataTables' : {
+			exports : 'dataTable'
+		},
+		'dataTables.sorting' : {
+			deps : [ 'dataTables' ]
+		},
+		'dataTables.bootstrap' : {
+			deps : [ 'dataTables' ]
+		},
+		'bootstrap' : {
+			deps : [ 'jquery' ]
+		},
+		'bootstrap-timepicker' : {
+			deps : [ 'bootstrap' ]
+		},
+		'jquery.fineuploader' : {
+			deps : [ 'jquery' ]
+		},
+		'select2' : {
+			deps : [ 'jquery' ]
+		},
+		'stepy' : {
+			deps : [ 'jquery' ]
+		},
+		'flot' : {
+			deps : [ 'jquery' ]
+		},
+		'flot.categories' : {
+			deps : [ 'flot' ]
+		},
+		'flot.time' : {
+			deps : [ 'flot' ]
+		},
+		'flot.pie' : {
+			deps : [ 'flot' ]
+		},
+		'qtip2' : {
+			deps : [ 'jquery' ]
+		},
+		'backbone.syphon' : {
+			deps : [ 'backbone' ]
+		},
+		'pickadate.picker.time' : {
+			deps : [ 'pickadate.picker' ]
+		},
+		'fusioncharts' : {
+			deps : [ 'jquery' ]
+		}
+	}
 });
 
-require([
+require([ 'app', 'router', 'backbone', 'jquery.ui', 'jquery.validate', 'jquery.form', 'jquery.actual', 'bootstrap', 'bootstrap-timepicker', 'jquery.serializeObject', 'dataTables',
+		'dataTables.sorting', 'dataTables.bootstrap', 'antiscroll', 'jquery.fineuploader', 'select2', 'stepy', 'flot', 'flot.categories', 'flot.time', 'flot.pie', 'qtip2',
+		'backbone.syphon', 'jquery.inputmask', 'moment', 'gebo_common', 'pickadate.picker.time', 'fusioncharts' ], function(app, Router) {
 
-'app','router','backbone','jquery.ui','jquery.validate','jquery.form','jquery.actual','bootstrap','bootstrap-timepicker',
-    'jquery.serializeObject',
-'dataTables','dataTables.sorting','dataTables.bootstrap','antiscroll','jquery.fineuploader',
-'select2','stepy','flot','flot.categories','flot.time','flot.pie','qtip2',
-'backbone.syphon','jquery.inputmask','moment','gebo_common','pickadate.picker.time','fusioncharts'], function(app,Router) {
+	$.fn.dataTableExt.oApi.fnMultiFilter = function(oSettings, oData) {
+		for ( var key in oData) {
+			if (oData.hasOwnProperty(key)) {
+				for (var i = 0, iLen = oSettings.aoColumns.length; i < iLen; i++) {
+					if (oSettings.aoColumns[i].mData == key) {
+						/* Add single column filter */
+						oSettings.aoPreSearchCols[i].sSearch = oData[key];
+						break;
+					}
+				}
+			}
+		}
+		this.oApi._fnReDraw(oSettings);
+	};
 
-    $.fn.dataTableExt.oApi.fnMultiFilter = function( oSettings, oData ) {
-        for ( var key in oData )
-        {
-            if ( oData.hasOwnProperty(key) )
-            {
-                for ( var i=0, iLen=oSettings.aoColumns.length ; i<iLen ; i++ )
-                {
-                    if( oSettings.aoColumns[i].mData == key )
-                    {
-                        /* Add single column filter */
-                        oSettings.aoPreSearchCols[ i ].sSearch = oData[key];
-                        break;
-                    }
-                }
-            }
-        }
-        this.oApi._fnReDraw( oSettings );
-    };
+	setTimeout('$("html").removeClass("js")', 500);
+	// $('#constructionModal').modal();
+	window.app = app;
+	app.router = new Router();
+	/*
+	 * $(document).on("click", "a[data-parent]", function(evt) { var href = {
+	 * prop: $(this).prop("href"), attr: $(this).attr("href") }; var root =
+	 * location.protocol + "//" + location.host + app.root;
+	 * console.log('href'+href); evt.preventDefault(); if (href.prop &&
+	 * href.prop.slice(0, root.length) === root) {
+	 * 
+	 * Backbone.history.navigate(href.attr, true); } });
+	 */
 
- setTimeout('$("html").removeClass("js")',500);
-// $('#constructionModal').modal();
- window.app = app;
- app.router = new Router();
-/* $(document).on("click", "a[data-parent]", function(evt) {
-  var href = { prop: $(this).prop("href"), attr: $(this).attr("href") };
-  var root = location.protocol + "//" + location.host + app.root;
-  console.log('href'+href);
-    evt.preventDefault();
-  if (href.prop && href.prop.slice(0, root.length) === root) {
-  
-    Backbone.history.navigate(href.attr, true);
-  }
-});*/
-
-  // Trigger the initial route and enable HTML5 History API support, set the
-  // root folder to '/' by default. Change in app.js.
-  Backbone.history.start();
- 
+	// Trigger the initial route and enable HTML5 History API support, set the
+	// root folder to '/' by default. Change in app.js.
+	Backbone.history.start();
 
 });
--- public/app/router.privia.js
+++ public/app/router.privia.js
@@ -1,96 +1,89 @@
-define([
-  'jquery',
-  'underscore',
-  'backbone',
-  './views/reportView',
-  './views/commonView',
-  './views/modalView',
-  './views/master/itemView'
-  ], 
+define([ 'jquery', 'underscore', 'backbone', './views/reportView', './views/commonView', './views/modalView', './views/master/itemView' ],
 
-  function($, _, Backbone, ReportView,CommonView, ModalView,ItemView) {
+function($, _, Backbone, ReportView, CommonView, ModalView, ItemView) {
 
-    var AppRouter = Backbone.Router.extend({
-      initialize : function(option) {
-        this.currentView = {};
-          this.bind( "route", function(){
-             if(hideSidebar) {
-                 $('body').addClass('sidebar_hidden');
-             }
-          });
-      },
-      routes: {
-      // Define some URL routes
-      'reportDashboard': 'showReportDashboard',	  
-      'reportTransaction': 'showReportTransaction',
-      'reportCashierClosing': 'showReportCashierClosing',
-      'reportLoginAudit': 'showReportLoginAudit',
-      'reportPL': 'showReportPL',
-      'modal/customer': 'addCustomerModal',
-      'home': 'showHome',
-      'analisis': 'showAnalisa',
-      'login': 'showLogin',
-      'success': 'showSuccess',
-      'master/item/list': 'itemList',
-      'master/item/upload': 'itemUpload',
-      'master/item/edit/:id': 'editItem',
-      'master/item': 'addItem',
-      // Default
-      '': 'defaultAction',
-      '*actions': 'defaultAction'
-    },
-    defaultAction: function(action) {
-      console.log('nodonk' + action);
-      new CommonView().render();
-    },
-    showHome: function() {
-      console.log('showHome');
-      new CommonView().render();
-    },
-    itemList: function() {
-      console.log('itemList');
-      new ItemView().render('list');
-    },
-    itemUpload: function() {
-      console.log('itemUpload');
-      new ItemView().render('upload');
-    },
-    addItem: function() {
-      console.log('addItem');
-      new ItemView().render('form');
-    },
-    editItem: function(id) {
-      console.log('editItem');
-      new ItemView({id:id}).render('form');
-    },
-    showReportDashboard: function() {
-       console.log('showReportDashboard');
-       new ReportView().render('reportDashboard');
-    },
-    showReportTransaction: function() {
-      console.log('showReportTransaction');
-      new ReportView().render('reportTransaction');
-    },
-    showReportCashierClosing: function() {
-        console.log('showReportCashierClosing');
-        new ReportView().render('reportCashierClosing');
-    },
-    showReportLoginAudit: function() {
-        console.log('showReportLoginAudit');
-        new ReportView().render('reportLoginAudit');
-    },
-    showReportPL: function() {
-        console.log('showReportPL');
-        new ReportView().render('reportPL');
-    },
-showSuccess: function() {
-  new SuccessView().render()
-},
-showLogin: function() {
-  console.log('showLogin');
-  new LoginView().render()
-}
-});
+	var AppRouter = Backbone.Router.extend({
+		initialize : function(option) {
+			this.currentView = {};
+			this.bind("route", function() {
+				if (hideSidebar) {
+					$('body').addClass('sidebar_hidden');
+				}
+			});
+		},
+		routes : {
+			// Define some URL routes
+			'reportDashboard' : 'showReportDashboard',
+			'reportTransaction' : 'showReportTransaction',
+			'reportCashierClosing' : 'showReportCashierClosing',
+			'reportLoginAudit' : 'showReportLoginAudit',
+			'reportPL' : 'showReportPL',
+			'modal/customer': 'addCustomerModal',
+			'home' : 'showHome',
+			'login' : 'showLogin',
+			'success' : 'showSuccess',
+			'master/item/list' : 'itemList',
+			'master/item/upload' : 'itemUpload',
+			'master/item/edit/:id' : 'editItem',
+			'master/item' : 'addItem',
+			// Default
+			'' : 'defaultAction',
+			'*actions' : 'defaultAction'
+		},
+		defaultAction : function(action) {
+			console.log('nodonk' + action);
+			new CommonView().render();
+		},
+		showHome : function() {
+			console.log('showHome');
+			new CommonView().render();
+		},
+		itemList : function() {
+			console.log('itemList');
+			new ItemView().render('list');
+		},
+		itemUpload : function() {
+			console.log('itemUpload');
+			new ItemView().render('upload');
+		},
+		addItem : function() {
+			console.log('addItem');
+			new ItemView().render('form');
+		},
+		editItem : function(id) {
+			console.log('editItem');
+			new ItemView({
+				id : id
+			}).render('form');
+		},
+		showReportDashboard : function() {
+			console.log('showReportDashboard');
+			new ReportView().render('reportDashboard');
+		},
+		showReportTransaction : function() {
+			console.log('showReportTransaction');
+			new ReportView().render('reportTransaction');
+		},
+		showReportCashierClosing : function() {
+			console.log('showReportCashierClosing');
+			new ReportView().render('reportCashierClosing');
+		},
+		showReportLoginAudit : function() {
+			console.log('showReportLoginAudit');
+			new ReportView().render('reportLoginAudit');
+		},
+		showReportPL : function() {
+			console.log('showReportPL');
+			new ReportView().render('reportPL');
+		},
+		showSuccess : function() {
+			new SuccessView().render()
+		},
+		showLogin : function() {
+			console.log('showLogin');
+			new LoginView().render()
+		}
+	});
 
-return AppRouter;
+	return AppRouter;
 });
