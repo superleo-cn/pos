647da35d4c5f953a1bce641febef6bd18583b711
LEO HU <superleo.peking@gmail.com>
2014-12-10 00:34:03 +0800

    OK

--- app/models/ReportTransactionSummary.java
+++ app/models/ReportTransactionSummary.java
@@ -69,6 +69,7 @@ public class ReportTransactionSummary implements Comparable<ReportTransactionSum
 				}
 			}
 		}
+		expList.orderBy("position asc");
 
 		PagingList<ReportTransactionDetail> pagingList = expList.findPagingList(pagination.pageSize);
 		pagingList.setFetchAhead(false);
